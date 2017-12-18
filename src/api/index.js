1ae1b03e4b681056ae4a293bea2d75f4b70c1653
Mannarino <frontend.moises@gmail.com>
2017-12-18 04:09:00 -0300

    Delete index.js

--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=3a7d7393cac5f1ecbda28077c96e9032&format=json`;
-
-export default function getArtist(country){
-	const url= URL.replace(':country',country)
-   return	fetch(url)
-		.then(res => res.json())
-		.then(json => json.topartists.artist)
-}
